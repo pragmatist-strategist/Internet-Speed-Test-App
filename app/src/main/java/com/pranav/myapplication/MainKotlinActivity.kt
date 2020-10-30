24d56c27a4e69ed6f55bf3a74f422fb1ad45c109
Andi Wijaya <andiwijayaid@gmail.com>
2020-10-30 23:49:06 +0800

    bind view with kotlin android extension in MainKotlinActivity.kt

--- app/src/main/java/com/pranav/myapplication/MainKotlinActivity.kt
+++ app/src/main/java/com/pranav/myapplication/MainKotlinActivity.kt
@@ -3,48 +3,27 @@ package com.pranav.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.pranav.myapplication.InternetSpeedBuilder.OnEventInternetSpeedListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainKotlinActivity : AppCompatActivity() {
 
-    private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: Adapter
-
-    companion object {
-        lateinit var resultlayout: LinearLayout
-        lateinit var retest: Button
-
-        lateinit var dspeed: TextView
-        lateinit var uspeed: TextView
-        lateinit var results: TextView
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
-        recyclerView = findViewById(R.id.recyclerview)
-        dspeed = findViewById(R.id.dspeed)
-        uspeed = findViewById(R.id.uspeed)
-        results = findViewById(R.id.result)
-        resultlayout = findViewById(R.id.resultview)
-        retest = findViewById(R.id.retest)
-
-        results.visibility = View.GONE
-        resultlayout.visibility = View.GONE
+        result.visibility = View.GONE
+        resultview.visibility = View.GONE
         retest.visibility = View.GONE
 
         adapter = Adapter()
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = adapter
+        recyclerview.layoutManager = LinearLayoutManager(this)
+        recyclerview.adapter = adapter
+
         val builder = InternetSpeedBuilder(this)
 
         builder.setOnEventInternetSpeedListener(object : OnEventInternetSpeedListener {
