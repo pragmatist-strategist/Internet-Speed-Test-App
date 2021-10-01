9cba0f9f6a97de755cca8ada2849eb1121265230
Pranav Sharma <31154698+pragmatist-strategist@users.noreply.github.com>
2021-10-01 09:35:47 +0530

    Update MainKotlinActivity.kt

--- app/src/main/java/com/pranav/myapplication/MainKotlinActivity.kt
+++ app/src/main/java/com/pranav/myapplication/MainKotlinActivity.kt
@@ -5,3 +5,25 @@ import android.support.v7.app.AppCompatActivity
 class MainKotlinActivity : AppCompatActivity(){
 
 }
+// int sum(Node* root, int k) 
+// { 
+//   //inorder traversal
+//   stack<Node*>s;
+//   vector<int>v;
+//  int count=0,sum=0;
+//  while (count<k)
+//     {
+//         while (root!=  NULL)
+//         {
+//             s.push(root);
+//             root = root->left;
+//         }
+//         root = s.top();
+//         s.pop();
+//         count++;
+//         sum+= root->data;
+//         root = root->right;
+//         if(count==k)
+//         return sum;
+//     } 
+// } 
